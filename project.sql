3f6954e9e34f645666a4e47d66d55bbe66a992b3
SinghBavandeep <115554428+SinghBavandeep@users.noreply.github.com>
2023-07-07 13:35:08 +0100

    MAJ SQL

--- project.sql
+++ project.sql
@@ -2,8 +2,8 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : jeu. 06 juil. 2023 à 13:58
+-- HÃ´te : 127.0.0.1:3306
+-- GÃ©nÃ©rÃ© le : ven. 07 juil. 2023 Ã  12:34
 -- Version du serveur : 8.0.31
 -- Version de PHP : 8.0.26
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `project`
+-- Base de donnÃ©es : `project`
 --
 
 -- --------------------------------------------------------
@@ -29,20 +29,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` text NOT NULL,
-  `pseudo` text NOT NULL,
-  `email` text NOT NULL,
-  `mdp` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+                                       `id` int NOT NULL AUTO_INCREMENT,
+                                       `nom` text NOT NULL,
+                                       `pseudo` text NOT NULL,
+                                       `email` text NOT NULL,
+                                       `mdp` text NOT NULL,
+                                       PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `admin`
+-- DÃ©chargement des donnÃ©es de la table `admin`
 --
 
 INSERT INTO `admin` (`id`, `nom`, `pseudo`, `email`, `mdp`) VALUES
-(1, 'admin', 'admin', 'admin@gmail.com', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/');
+    (1, 'admin', 'admin', 'admin@gmail.com', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/');
 
 -- --------------------------------------------------------
 
@@ -52,23 +52,23 @@ INSERT INTO `admin` (`id`, `nom`, `pseudo`, `email`, `mdp`) VALUES
 
 DROP TABLE IF EXISTS `bill`;
 CREATE TABLE IF NOT EXISTS `bill` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ide` int NOT NULL,
-  `idv` int NOT NULL,
-  `dateD` date NOT NULL,
-  `dateF` date NOT NULL,
-  `valeur` int NOT NULL,
-  `etatR` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+                                      `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+                                      `ide` int NOT NULL,
+                                      `idv` int NOT NULL,
+                                      `dateD` date NOT NULL,
+                                      `dateF` date NOT NULL,
+                                      `valeur` int NOT NULL,
+                                      `etatR` tinyint(1) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `bill`
+-- DÃ©chargement des donnÃ©es de la table `bill`
 --
 
 INSERT INTO `bill` (`id`, `ide`, `idv`, `dateD`, `dateF`, `valeur`, `etatR`) VALUES
-(1, 1, 1, '2021-10-15', '2021-10-15', 1000, 0),
-(2, 2, 1, '2021-10-14', '2021-10-14', 1000, 0);
+                                                                                 (1, 1, 1, '2021-10-15', '2021-10-15', 1000, 0),
+                                                                                 (2, 2, 1, '2021-10-14', '2021-10-14', 1000, 0);
 
 -- --------------------------------------------------------
 
@@ -78,22 +78,22 @@ INSERT INTO `bill` (`id`, `ide`, `idv`, `dateD`, `dateF`, `valeur`, `etatR`) VAL
 
 DROP TABLE IF EXISTS `company`;
 CREATE TABLE IF NOT EXISTS `company` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nom` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `adresse` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+                                         `id` int NOT NULL AUTO_INCREMENT,
+                                         `nom` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                         `adresse` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                         PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `company`
+-- DÃ©chargement des donnÃ©es de la table `company`
 --
 
 INSERT INTO `company` (`id`, `nom`, `adresse`) VALUES
-(1, 'Etransport', '1 adresse Etransport'),
-(2, 'VIPtravel', '2 adresse VIPtravel'),
-(3, 'RedDrive', '3 adresse RedDrive'),
-(4, 'SunMusic', '4 adresse SunMusic'),
-(5, 'Toyota', '5 adresse Toyota');
+                                                   (1, 'Etransport', '1 adresse Etransport'),
+                                                   (2, 'VIPtravel', '2 adresse VIPtravel'),
+                                                   (3, 'RedDrive', '3 adresse RedDrive'),
+                                                   (4, 'SunMusic', '4 adresse SunMusic'),
+                                                   (5, 'Toyota', '5 adresse Toyota');
 
 -- --------------------------------------------------------
 
@@ -103,27 +103,27 @@ INSERT INTO `company` (`id`, `nom`, `adresse`) VALUES
 
 DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `nom` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `pseudo` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `mdp` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `email` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `idE` int NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+                                          `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+                                          `nom` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                          `pseudo` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                          `mdp` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                          `email` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                          `idE` int NOT NULL,
+                                          PRIMARY KEY (`id`),
+    UNIQUE KEY `id` (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `customer`
+-- DÃ©chargement des donnÃ©es de la table `customer`
 --
 
 INSERT INTO `customer` (`id`, `nom`, `pseudo`, `mdp`, `email`, `idE`) VALUES
-(2, 'Voyer', 'Avoyer75', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'voyer@email.com', 2),
-(3, 'Burban', 'Cburban92', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'burban@email.com', 3),
-(4, 'Agez', 'fAgez03', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'agez@email.com', 1),
-(5, 'Dupont ', 'dupont', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'dupont@gmail.com', 1),
-(6, 'Perrier ', 'perrier13', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'perrier@email.com', 4),
-(13, 'Durant', 'durant', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'durant@gmail.com', 1);
+                                                                          (2, 'Voyer', 'Avoyer75', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'voyer@email.com', 2),
+                                                                          (3, 'Burban', 'Cburban92', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'burban@email.com', 3),
+                                                                          (4, 'Agez', 'fAgez03', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'agez@email.com', 1),
+                                                                          (5, 'Dupont ', 'dupont', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'dupont@gmail.com', 1),
+                                                                          (6, 'Perrier ', 'perrier13', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'perrier@email.com', 4),
+                                                                          (13, 'Durant', 'durant', '$6$rounds=5000$anexamplestringf$cganXrEeVcQ.SuZ6zs.ptb5ulJ9chgHT69ENOJ7A9RPikHEFDgs2Yeq1PdeWciEvhGev2fbnL9bxv7H.546rS/', 'durant@gmail.com', 1);
 
 -- --------------------------------------------------------
 
@@ -133,32 +133,33 @@ INSERT INTO `customer` (`id`, `nom`, `pseudo`, `mdp`, `email`, `idE`) VALUES
 
 DROP TABLE IF EXISTS `vehicle`;
 CREATE TABLE IF NOT EXISTS `vehicle` (
-  `ref` int NOT NULL AUTO_INCREMENT,
-  `nom` text NOT NULL,
-  `type` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `caract` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `details` text,
-  `state` tinyint(1) DEFAULT '0',
-  `img` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
-  `prixJ` double NOT NULL,
-  `prixM` double NOT NULL,
-  `debutL` date DEFAULT NULL,
-  `finL` date DEFAULT NULL,
-  `idL` int NOT NULL DEFAULT '1',
-  `idC` int DEFAULT NULL,
-  PRIMARY KEY (`ref`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+                                         `ref` int NOT NULL AUTO_INCREMENT,
+                                         `nom` text NOT NULL,
+                                         `type` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                         `caract` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+                                         `details` text,
+                                         `state` tinyint(1) DEFAULT '0',
+    `img` text CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+    `prixJ` double NOT NULL,
+    `prixM` double NOT NULL,
+    `debutL` date DEFAULT NULL,
+    `finL` date DEFAULT NULL,
+    `idL` int NOT NULL DEFAULT '1',
+    `idC` int DEFAULT NULL,
+    PRIMARY KEY (`ref`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `vehicle`
+-- DÃ©chargement des donnÃ©es de la table `vehicle`
 --
 
 INSERT INTO `vehicle` (`ref`, `nom`, `type`, `caract`, `details`, `state`, `img`, `prixJ`, `prixM`, `debutL`, `finL`, `idL`, `idC`) VALUES
-(1, 'BMW X5', 'Diesel', 'Nombre de places : 5, \nBoÃ®te de vitesse : automatique, \nMoteur : 3.0d381', 'La BMW W5 faisant partie de notre catÃ©gorie moyenne est une voiture familiale avec un confort et une fiabilitÃ© excellente.', 1, 'BMW X5 (moyen).png', 19.99, 449.99, NULL, NULL, 1, 5),
-(2, 'Ferrari 488', 'Diesel', 'Nombre de places : 2, \nBoÃ®te de vitesse : manuelle, \nMoteur : V8', 'La Ferrari 488 faisant partie de notre catÃ©gorie luxe est une voiture de sport trÃ¨s rapide avec une excellente tenue de route.', 0, 'Ferrari-488 (luxe).png', 83.99, 2499.99, NULL, NULL, 1, 2),
-(3, 'Dacia Duster', 'Diesel', 'Nombre de places : 5\nBoÃ®te de vitesse : automatique\nMoteur : 3.0d381', 'La Dacia Duster faisant partie de notre catÃ©gorie moyenne est une voiture familiale avec un confort et une fiabilitÃ© excellente.', 1, 'dacia duster (low coast).png', 14.99, 399.99, NULL, NULL, 1, 13),
-(4, 'Fiat Multipla', 'DIesel', 'Nombre de places : 6\nBoÃ®te de vitesse : manuelle\nMoteur : 1.6 16v', 'La Fiat Multipla faisant partie de notre catÃ©gorie low coast est une voiture adaptÃ©e aux petits budgets permettant de faire des trajets quotidiens.', 0, 'Fiat Multipla (low coast).png', 9.99, 249.99, NULL, NULL, 1, 3),
-(5, 'Lamborghini Gallardo', 'Diesel', 'Nombre de place : 2\nBoÃ®te de vitesse : manuelle\nMoteur : V10', 'La Lamborghini Gallardo faisant partie de notre catÃ©gorie luxe est une voiture de sport trÃ¨s rapide avec une excellente tenue de route.', 1, 'lamborghini gallardo (luxe).png', 99.99, 2699.99, NULL, NULL, 1, 4);
+                                                                                                                                        (1, 'BMW X5', 'Diesel', 'Number of seats: 5,\r\nGearbox: automatic,\r\nEngine: 3.0d381', 'The BMW W5 in our medium category is a family car with excellent comfort and reliability.', 1, 'BMW X5 (moyen).png', 19.99, 449.99, NULL, NULL, 1, 5),
+                                                                                                                                        (2, 'Ferrari 488', 'Diesel', 'Number of seats: 2,\r\nGearbox: manual,\r\nEngine: V8', 'The Ferrari 488 in our luxury category is a very fast sports car with excellent handling.', 0, 'Ferrari-488 (luxe).png', 83.99, 2499.99, NULL, NULL, 1, 2),
+                                                                                                                                        (3, 'Dacia Duster', 'Diesel', 'Number of seats: 5\r\nGearbox: automatic\r\nEngine: 3.0d381', 'The Dacia Duster in our medium category is a family car with excellent comfort and reliability.', 1, 'dacia duster (low coast).png', 14.99, 399.99, NULL, NULL, 1, 13),
+                                                                                                                                        (4, 'Fiat Multipla', 'DIesel', 'Number of seats: 6\r\nGearbox: manual\r\nEngine: 1.6 16v', 'The Fiat Multipla belonging to our low coast category is a car adapted to small budgets allowing to make daily journeys.', 0, 'Fiat Multipla (low coast).png', 9.99, 249.99, NULL, NULL, 1, 3),
+                                                                                                                                        (5, 'Lamborghini Gallardo', 'Diesel', 'Number of places: 2\r\nGearbox: manual\r\nEngine: V10', 'The Lamborghini Gallardo in our luxury category is a very fast sports car with excellent handling.', 1, 'lamborghini gallardo (luxe).png', 99.99, 2699.99, NULL, NULL, 1, 4);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
